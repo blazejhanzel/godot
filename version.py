dcae29c4d4f4371b824c9bb2555eab1b4eb0927c
Błażej Hanzel <29534847+blazejhanzel@users.noreply.github.com>
2025-10-13 22:01:23 +0200

    uploaded custom build
    # Conflicts:
    #	.github/actions/godot-deps/action.yml
    #	.github/workflows/android_builds.yml
    #	.github/workflows/linux_builds.yml
    #	version.py

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
 major = 4
-minor = 6
+minor = 5
 patch = 0
-status = "dev"
+status = "custom"
 module_config = ""
 website = "https://godotengine.org"
-docs = "latest"
+docs = "4.5"
